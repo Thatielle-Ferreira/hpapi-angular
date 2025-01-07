3c7dc7aa2384952c7632221f1c60a6d458c45c4c
Thatielle Ferreira <thatielle.pires@bravolog.com.br>
2025-01-07 11:56:51 -0300

    Adicionando paginação na grid de personagens

--- src/app/modules/personagens/personagens.component.ts
+++ src/app/modules/personagens/personagens.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit                  } from '@angular/core';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatInputModule                     } from '@angular/material/input';
-import { MatFormFieldModule                 } from '@angular/material/form-field';
-import { HpapiService                       } from '../../services/hpapi.service';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule  } from '@angular/material/table';
+import { MatInputModule                      } from '@angular/material/input';
+import { MatFormFieldModule                  } from '@angular/material/form-field';
+import { HpapiService                        } from '../../services/hpapi.service';
+import { MatPaginator, MatPaginatorModule    } from '@angular/material/paginator';
 
 export interface CharactersData {
+  id             : number;
   name           : string;
   actor          : string;
   gender         : string;
@@ -14,43 +16,49 @@ export interface CharactersData {
   wizard         : string;
   patronus       : string;
   wand           : string;
-  hogwartsStaff  : string;
-  hogwartsStudent: string;
+  hogwartsStaff  : boolean;
+  hogwartsStudent: boolean;
 }
 
 @Component({
   selector   : 'app-personagens',
-  imports    : [ MatFormFieldModule, MatInputModule, MatTableModule ],
+  imports    : [ MatFormFieldModule, MatInputModule, MatTableModule, MatPaginatorModule ],
   templateUrl: './personagens.component.html',
   styleUrl   : './personagens.component.scss'
 })
-export class PersonagensComponent implements OnInit {
+export class PersonagensComponent implements AfterViewInit {
 
-  displayedColumns: string[] = ['name', 'actor', 'gender', 'ancestry', 'house', 'species', 'wizard', 'patronus', 'wand', 'hogwartsStaff', 'hogwartsStudent'];
-  dataSource: MatTableDataSource<CharactersData>;
+  displayedColumns: string[] = ['id', 'name', 'actor', 'gender', 'ancestry', 'house', 'species', 'wizard', 'patronus', 'wand', 'hogwartsStaff', 'hogwartsStudent'];
+  dataSource      = new MatTableDataSource<CharactersData>([]);
 
-  arCharacters: any;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor (
     private hpapiService: HpapiService
   ) {
     this.getPersonagens();
-    this.dataSource = new MatTableDataSource(this.arCharacters);
   }
 
-  ngOnInit() { }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   getPersonagens = async () => {
 
     this.hpapiService.get('listarPersonagens').subscribe({
       next: (result) => {
-        this.arCharacters = result;
-        this.dataSource   = new MatTableDataSource(this.arCharacters);
+        result.map((item: any, index: number) => item.id = index + 1);
+        this.dataSource.data = result;
+        this.dataSource.paginator = this.paginator;
       },
       error: (erro) => console.error(erro)
     });
